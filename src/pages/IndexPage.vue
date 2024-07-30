a00468c177ebdce639e797a931b02709a7bd7f55
fang-tanbamrung <watcharakorn.ta@logicspark.com>
2024-07-30 13:29:09 +0700

    init and add vpv package

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { VPdfViewer, useLicense } from '@vue-pdf-viewer/viewer';
+
+const licenseKey = import.meta.env.VITE_VPV_LICENSE ?? 'your-VPV-license-key';
+useLicense({ licenseKey });
+const pdfFileSource =
+  'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf';
+</script>
+<template>
+  <div :style="{ width: '1028px', height: '700px' }">
+    <VPdfViewer :src="pdfFileSource" />
+  </div>
+</template>
